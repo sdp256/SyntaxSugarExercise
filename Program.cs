703dd4c97949fea7dd7343c5fe194d7f6bfe1169
sdp256 <steevnpairy@gmail.com>
2021-02-24 19:46:49 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
